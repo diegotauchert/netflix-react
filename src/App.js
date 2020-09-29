c2877ec606a4967c609991f58a0514c9071a6ffb
Diego Tauchert <diego.tauchert@gmail.com>
2020-09-29 19:18:07 -0300

    setinterval featured data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import FeaturedMovie from "./components/FeaturedMovie";
 import Header from './components/Header';
-import './App.css'
+import './App.css';
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
@@ -16,20 +16,34 @@ export default () => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
-
-      //filtrando apenas os filmes originais netflix
-      let originals = list.filter(i => i.slug === 'originals');
-      // gerando um numero aleatorio de acordo com o objeto de filmes originais
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
-      // pegando um filme aleatorio a partir do numero randomico
-      let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
-      setFeaturedData(chosenInfo);
-      //console.log(originals, randomChosen, chosen, chosenInfo);
     };
-
+    
     loadAll();
   }, []);
+
+  useEffect(()=>{
+    
+    const loadFeatured = async () => {
+        //filtrando apenas os filmes originais netflix
+        let originals = movieList.filter(i => i.slug === 'originals');
+        // gerando um numero aleatorio de acordo com o objeto de filmes originais
+        let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+        // pegando um filme aleatorio a partir do numero randomico
+        let chosen = originals[0].items.results[randomChosen];
+        let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+        setFeaturedData(chosenInfo);
+    };
+    
+    {movieList.length > 0 && 
+      loadFeatured();
+    }
+    
+    const interval = setInterval(() => {
+      loadFeatured();
+    }, 5000);
+    return () => clearInterval(interval);
+
+  },[movieList]);
 
   useEffect(()=>{
     const scrollListener = () => {
